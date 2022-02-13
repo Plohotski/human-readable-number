4fdb7f2ab394cfe4776e238bcc1ae2baed9485e8
Plohocky Artom <plohoc@gmail.com>
2022-02-13 13:37:47 +0300

    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let readableNumber = ''
+    const one = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const ten = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    const tenNineteen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    if (number === 0) {
+        return one[0]
+    }
+
+    if (Math.trunc(number / 100))
+        readableNumber = one[Math.trunc(number / 100)] + ' hundred'
+
+    if ((number % 100 < 20) && (number % 100 > 9)) {
+        if (readableNumber != '')
+            readableNumber = readableNumber + ' '
+        return readableNumber + tenNineteen[number % 10]
+    }
+
+    if (number % 100 > 10) {
+        if (readableNumber != '')
+            readableNumber = readableNumber + ' '
+        readableNumber = readableNumber + ten[(number % 100 - number % 10) / 10]
+    }
+
+    if (number % 10) {
+        if (readableNumber != '')
+            readableNumber = readableNumber + ' '
+        readableNumber = readableNumber + one[number % 10]
+    }
+    return readableNumber;
 }
